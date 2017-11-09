71c4a6a055ccebdb735af79d0bc3531427e74dc2
Isabel Costa <isabelcmdcosta@gmail.com>
2017-11-09 15:56:01 +0000

    more code style fixes | layout simplifications

--- app/src/main/java/com/example/isabelcosta/moviesapp/data/source/remote/RemoteHelper.kt
+++ app/src/main/java/com/example/isabelcosta/moviesapp/data/source/remote/RemoteHelper.kt
@@ -1,7 +1,11 @@
 package com.example.isabelcosta.moviesapp.data.source.remote
 
+// Remote constants
+
 const val API_VERSION = "3"
 const val MOVIES_API_ENDPOINT = "https://api.themoviedb.org/$API_VERSION/"
 const val IMAGES_API_ENDPOINT = "https://image.tmdb.org/t/p/w500"
 
-val getFullImageUrl: (String) -> String = {imageName -> "$IMAGES_API_ENDPOINT$imageName"}
+// Helpful methods
+
+fun getFullImageUrl(imageName: String): String = "$IMAGES_API_ENDPOINT$imageName"
