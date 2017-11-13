571c3b29540a139cd20eb8ae3e4bea525486bd27
Isabel Costa <isabelcmdcosta@gmail.com>
2017-11-13 17:06:08 +0000

    added navigation side menu work in progress

--- app/src/main/java/com/example/isabelcosta/moviesapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/isabelcosta/moviesapp/ui/activities/MainActivity.kt
@@ -2,17 +2,46 @@ package com.example.isabelcosta.moviesapp.ui.activities
 
 import android.os.Bundle
 import com.example.isabelcosta.moviesapp.R
+import com.example.isabelcosta.moviesapp.ui.fragments.BaseFragment
 import com.example.isabelcosta.moviesapp.ui.fragments.NowPlayingMoviesFragment
 import com.example.isabelcosta.moviesapp.utils.replaceActivityFragment
+import kotlinx.android.synthetic.main.main_drawer_layout.*
 
 class MainActivity : BaseActivity() {
 
+    private val fragmentsMenuMap: HashMap<Int, BaseFragment<MainActivity>> = hashMapOf(
+            R.id.menuNowPlayingMovies to NowPlayingMoviesFragment.newInstance(),
+            R.id.menuPopularTvShows to NowPlayingMoviesFragment.newInstance(),
+            R.id.menuSearchMovies to NowPlayingMoviesFragment.newInstance()
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        populateSideMenu()
+
         replaceActivityFragment(fragmentManager, R.id.mainActivityFrameLayout, NowPlayingMoviesFragment.newInstance())
     }
 
     override fun getLayoutResourceId(): Int {
-        return R.layout.activity_main
+        return R.layout.main_drawer_layout
+    }
+
+    private fun populateSideMenu() {
+
+        supportActionBar?.setIcon(R.drawable.ic_menu_black_24dp)
+        
+        mainActivityNavigationView.setNavigationItemSelectedListener {
+            val selectedFragment = fragmentsMenuMap[it.itemId]
+
+            mainActivityDrawerLayout.closeDrawers()
+
+            selectedFragment?.let {
+                replaceActivityFragment(fragmentManager, R.id.mainActivityFrameLayout, it)
+            }
+
+            true
+        }
+//        mainActivityNavigationView.setCheckedItem(R.id.menuNowPlayingMovies)
     }
 }
