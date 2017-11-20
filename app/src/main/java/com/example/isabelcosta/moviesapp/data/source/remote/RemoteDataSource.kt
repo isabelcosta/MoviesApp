bc0f2de568179092b8804875e0043cdfba81a804
Isabel Costa <isabelcmdcosta@gmail.com>
2017-11-20 15:31:07 +0000

    close #18 retrofit logger

--- app/src/main/java/com/example/isabelcosta/moviesapp/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/example/isabelcosta/moviesapp/data/source/remote/RemoteDataSource.kt
@@ -2,15 +2,25 @@ package com.example.isabelcosta.moviesapp.data.remote
 
 import com.example.isabelcosta.moviesapp.MoviesApplication
 import com.example.isabelcosta.moviesapp.utils.MOVIES_API_ENDPOINT
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 abstract class RemoteDataSource {
 
     protected val apiKey: String = MoviesApplication.instance.MoviesDbApiKey
 
+    private val httpClient: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }).build()
+
     protected val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(MOVIES_API_ENDPOINT)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+            .baseUrl(MOVIES_API_ENDPOINT)
+            .client(httpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
 }
